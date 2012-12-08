9d647f7c2eefa4acd8b1c62910a1f79095019cb6
Toby DiPasquale <toby@trustmetrics.com>
2012-12-07 22:50:25 -0500

    clean up

--- agent/main.go
+++ agent/main.go
@@ -14,24 +14,21 @@ var sampleInterval int
 var collectorAddr string
 
 func init() {
-  flag.IntVar(&sampleInterval, "sample", 10, "sampling interval in seconds")
-  flag.StringVar(&collectorAddr, "collector", "", "address of collector service")
+  flag.IntVar(&sampleInterval, "t", 10, "sampling interval in seconds")
+  flag.StringVar(&collectorAddr, "c", "", "address of collector service")
 }
 
 func main() {
   flag.Parse()
   runtime.GOMAXPROCS(runtime.NumCPU())
-
   signalChan := make(chan os.Signal, 1)
   signal.Notify(signalChan, os.Interrupt, os.Kill)
-
   log.Printf("agent started: sampling every %d seconds\n", sampleInterval)
   ticker := time.NewTicker(time.Duration(sampleInterval) * time.Second)
   for {
     select {
     case <-ticker.C:
       linux.StandardStats()
-      break
     case s := <-signalChan:
       log.Printf("caught signal %s: shutting down\n", s)
       return
